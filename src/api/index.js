79492d23773454ef6babbbb58ef9323b01909e67
unknown <adamk3wilson@gmail.com>
2020-02-26 15:37:53 +0000

    Add api keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "<your key here>",
-  authDomain: "<your key here>",
-  databaseURL: "<your key here>",
-  projectId: "<your key here>",
-  storageBucket: "<your key here>",
-  messagingSenderId: "<your key here>",
-  appId: "<your key here>8"
+  apiKey: "AIzaSyC0nyosmEn0y6HSf6WLUJVQiY60WxIrrNA",
+  authDomain: "slackover-chat.firebaseapp.com",
+  databaseURL: "https://slackover-chat.firebaseio.com",
+  projectId: "slackover-chat",
+  storageBucket: "slackover-chat.appspot.com",
+  messagingSenderId: "840118303550",
+  appId: "1:840118303550:web:72941c88249d978f0c4a6f",
+  measurementId: "G-58F7JHB1XF"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
