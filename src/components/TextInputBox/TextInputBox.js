32003227d10796cb288f4ba973a9170d55e6aad3
Taylor <t.sutcliffe@ncl.ac.uk>
2020-02-26 17:51:19 +0000

    Add enter functionality

--- src/components/TextInputBox/TextInputBox.js
+++ src/components/TextInputBox/TextInputBox.js
@@ -1,11 +1,12 @@
-import React, { useState, useMemo } from 'react';
-import { createEditor, Node } from 'slate';
+import React, { useState, useMemo, useEffect } from 'react';
+import { createEditor, Node, Editor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 import { withHistory } from 'slate-history';
 
 const TextInputBox = ({ messagesApi, user }) => {
   const editor = useMemo(() => withHistory(withReact(createEditor())), [])
   const [newMessage, setNewMessage] = useState([ { children: [ { text: '' } ] } ]);
+  const [clear, setClear] = useState(false)
 
   /**
    * Parse the text provided by the input and send to the Firestore. Once the message has been sent, we can reset 
@@ -14,15 +15,41 @@ const TextInputBox = ({ messagesApi, user }) => {
    * @param {Event} e 
    */
   const onSubmit = (e) => {
-    e.preventDefault();
+    if (e) {
+      e.preventDefault(); 
+      e.stopPropagation();
+    }
+
     const text = parseText(newMessage);
 
     // Add the message to the database
     messagesApi.add({ message: text, createdAt: new Date(), userId: user.uid, sentBy: user.displayName, channelName: 'general' });
 
     // ... then we can reset state
-    setNewMessage([ { children: [ { text: '' } ] } ]);
+    
+    for(let i = 0; i < text.length + 1 ; i++){
+      Editor.deleteBackward(editor);
+    }
+    
+
+    //setClear(true)
   }
+
+  //useEffect(() => {
+  //  if (clear) {
+  //  setNewMessage([ { children: [ { text: '' } ] } ]);
+  //  setClear(false)
+  //  }
+  //  
+  //}, [clear])
+
+  const onKey = (e) => {
+    console.log(e.key);
+    if(e.key === 'Enter'){
+      onSubmit(e);
+    }
+  }
+
 
   /**
    * Since messages can have multiple paragraphs, return a joined string.
@@ -33,8 +60,9 @@ const TextInputBox = ({ messagesApi, user }) => {
     return nodes.map(n => Node.string(n)).join('\n')
   }
 
+
   return (
-    <div className={`textInput`}>
+    <div className={`textInput`} onKeyDown={onKey}>
       <Slate editor={editor} value={newMessage} onChange={value => {
           setNewMessage(value);
         }}>
